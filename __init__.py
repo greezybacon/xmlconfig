904f600321d498c33ac5e8afdec84ad02eb6a23f
greezybacon <gravydish@gmail.com>
2011-03-04 20:58:20 -0600

    Add license boilerplate Move todo list to its own file

--- __init__.py
+++ __init__.py
@@ -1,43 +1,41 @@
-#!/usr/bin/env python
 # encoding: utf-8
 """
-xmlconfig.py
+xmlconfig for Python
 
 Created by Jared Hancock on 2011-02-05.
-Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+Copyright (c) 2011 klopen Enterprises. All rights reserved.
 """
+
+# Copyright (c) 2011, Jared Hancock
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of klopen enterprises nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL KLOPEN ENTERPRISES BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import sys, os
 import re
 import urllib2
 from xml.sax import saxutils, handler, make_parser
 from decimal import Decimal
-
-# TODO:
-# Support wish-list
-# x Using a name for a config doc (ie. getConfig(name))
-# x list/dict support
-# x choose block support
-# x namespaces
-# x references, nested references
-# x binary content
-# x ecrypted content
-# x magic environment (env) namespace
-# - locking/unlocking documents with <cryptic> elements
-# - schema & validation script
-# - support password (key) for encrypted stuff
-# x imports (circular reference auto-correct)
-# x import changing local namespace or foreign document
-# ? references to non-imported namespaces
-# x auto loading
-# - auto loading -- search in subfolders inside folder name 'config'
-#           and make 'config' folder name configurable
-# - auto updating (when file is modified)
-# - event notification of updates
-# - retrieve elements from config file with fall-back default value
-# - installer (distutils)
-# - installer creates unique host-key for en-/decryption
-# - write support (perhaps to be used with (un)locking)
 
 LOCAL_NAMESPACE="__local"
     
