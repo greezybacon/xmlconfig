27586a7e48ec5a4220c2a4f0dd411d34e3012345
greezybacon <gravydish@gmail.com>
2011-03-07 21:12:40 -0600

    Build a distutils script

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# (c) 2011 klopen Enterprises.  See LICENSE.txt file for details
+
+from distutils.core import setup
+
+setup(
+    name="xmlconfig",
+    license="BSD",
+    version="0.1.0",
+    description="xml-based configuration parser for Python",
+    author="Jared Hancock",
+    maintainer_email="gravydish@gmail.com",
+    url="http://code.google.com/p/xmlconfig/",
+    packages=["xmlconfig"],
+    scripts=["bin/xmlconfig"],
+    long_description="""xmlconfig is a feature rich configuration parser 
+    for the Python programming language providing a more flexable approach
+    to configuring daemon-based software. It provides complex configuration
+    options such as conditional elements, encrypted elements, distributed 
+    configuration, strong typing, and extensibility.""",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Aproved :: BSD License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Topic :: Security :: Cryptography",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Text Processing :: Markup :: XML"
+    ]
+)
