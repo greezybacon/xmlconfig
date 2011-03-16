c8c9e1eb9a1ffe4b476ca41dbfb0a414e41171ff
greezybacon <gravydish@gmail.com>
2011-03-15 23:16:35 -0500

    Avogadro spelling mistake

--- test/testTypes.py
+++ test/testTypes.py
@@ -58,17 +58,18 @@ def testTypeFloatExponent():
     u"""<?xml version="1.0" encoding="utf-8"?>
     <config>
         <constants>
-            <float key="avagadro">6.023E+23</float>
+            <float key="avogadro">6.023E+23</float>
         </constants>
     </config>
     """), LOCAL_NAMESPACE)
     from decimal import Decimal
-    print conf.get("avagadro")
-    assert conf.get("avagadro") == Decimal('6.023E+23')
+    print conf.get("avogadro")
+    assert conf.get("avogadro") == Decimal('6.023E+23')
 
     # Float element returns Decimal-s for precision
     from decimal import Decimal
     assert conf.get("float") == Decimal('3.14159265359')
+
 def testTypeBool():
     from xmlconfig import getConfig, LOCAL_NAMESPACE
     from core import stringIOWrapper
