0c96d21bf7ea16c3f1fee6e7337adedda61f9311
greezybacon <gravydish@gmail.com>
2011-03-23 17:26:12 -0500

    Move away from weird parser stack in favor of a strong XmlConfigDocument class. The main advantage is that the XmlConfigDocument makes a nice closure around the namespace it will be load. When a document is loaded that sources a second document, a new XmlConfigDocument will be created to handle loading the new document as well as store the namespace it was originally requested to be loaded into.

--- src/xmlconfig/__init__.py
+++ src/xmlconfig/__init__.py
@@ -17,6 +17,9 @@ def getConfig(name=""):
         __configs[name] = XmlConfig(name)
     return __configs[name]
 
+def clearConfigs():
+    __configs.clear()
+
 # Rectify differences between Python 2 and Python3k
 # - urllib2 / urllib.request
 # Allow unit test modules to override the definition of urlopen
@@ -77,38 +80,24 @@ class XmlConfigParser(handler.ContentHandler, object):
 
     def __init__(self, name=None, attrs=None, parser=None, parent=None, 
             namespace=None):
-        self.constants = {}
         self.parent = parent
-        self.parsers = []
         self._content = ""
-        self._parser = parser
         self._options = Options(self.default_options)
         self.type = name
         if attrs is not None: self.parse_options(attrs)
-        if parser is not None: self.push_parser(parser, namespace)
         if namespace is not None and not self.has_option("namespace"):
             self.options["namespace"] = namespace
         self.on_update = EventHook()
         self.on_added = EventHook()
         
-    def push_parser(self, parser, namespace=None):
-        self.parsers.append((parser,namespace))
-        
-    def pop_parser(self):
-        self.parsers.pop()
-        
     @property
     def parser(self):
-        if len(self.parsers) == 0:
-            return self.parent.parser
-        return self.parsers[-1][0]
-        
-    @property
-    def parser_namespace(self):
-        if len(self.parsers) == 0:
-            return self.parent.parser_namespace
-        return self.parsers[-1][1]
+        return self.parent.parser
 
+    def endElement(self, name):
+        if self.parser is not None:
+            self.parser.setContentHandler(self.parent)
+        
     def parse_options(self, attrs):
         self._options.merge(attrs)
         
@@ -129,52 +118,13 @@ class XmlConfigParser(handler.ContentHandler, object):
         
     def has_option(self, name):
         return name in self._options
-
-    def startElement(self, name, attrs):
-        if not name in self.content_types:
-            # Only enter into <constant> elements (or other registered
-            # supported content types)
-            return
-        # Keep namespace list simple
-        new_element = self.content_types[name](name=name, attrs=attrs,
-            parent=self, namespace=self.parser_namespace)
-        if new_element.namespace in self.constants:
-            new_element = self.constants[new_element.namespace]
-        else:
-            self.constants[new_element.namespace] = new_element
-        self.parser.setContentHandler(new_element)
-        
-    def endElement(self, name):
-        if name in self.content_types:
-            self.pop_parser()
-        
-    @property
-    def namespaces(self):
-        return list(self.constants.keys())
         
     @property
     def root(self):
         if self.parent is None:
             return self
         return self.parent.root
-        
-    def lookup(self, key, namespace=LOCAL_NAMESPACE):
-        # XXX A regex would make more sense here
-        split = key.split(self.namespace_separator, 1)
-        if len(split) == 2:
-            # XXX override given namespace?
-            namespace=split[0]
-            key=split[1]
-        else:
-            key=split[0]
-        if namespace == "env":
-            # Special lookup for environment variables
-            return os.environ[key]
-        # XXX self.constants should be a hashtable
-        return self.constants[namespace].lookup(key)
-
-        raise KeyError("{0}: Cannot lookup reference".format(key))
-                
+                        
     @classmethod
     def register_child(handler, name):
         def register(clas):
@@ -190,9 +140,6 @@ class XmlConfigParser(handler.ContentHandler, object):
         else:
             return self.parent.namespace
 
-    def link_namespace(self, namespace, newname):
-        self.constants[namespace].link_to(newname)
-
 class XmlConfig(XmlConfigParser):
     default_options = {
         'autoload-folder-name':     'config'
@@ -200,9 +147,8 @@ class XmlConfig(XmlConfigParser):
 
     def __init__(self, name):
         self._files = {}
-        self._links = {}
-        # Stuff from XmlConfigParser
-        self.parsers = []
+        self.links = {}
+        self.documents = {}
         self.constants = {}
         self.parent = None
         self.name = name
@@ -225,6 +171,8 @@ class XmlConfig(XmlConfigParser):
         """
         pass
 
+    # XXX: Move to XmlConfigDocument interface
+
     def load(self, url, namespace=LOCAL_NAMESPACE, for_import=False):
         # Keep track of loaded files to ward off circular dependencies. If
         # a file is requested to be loaded that is already, and it has not
@@ -240,12 +188,15 @@ class XmlConfig(XmlConfigParser):
             # reloading and importing, etc.
             self._original_url = url
         if url in self._files:
-            # Don't load if the file is alread loaded under a difference
+            # Don't load if the file is already loaded under a difference
             # namespace
             if self._files[url]['namespace'] != namespace:
                 # Already loaded this file, so just create a link to another
-                # namespace
-                self._links[self._files[url]['namespace']] = namespace
+                # namespace. This will allow two documents to import the same
+                # document into different namespaces. Doing so without links
+                # would create unnecessary imports and potential circular
+                # dependency issues
+                self.links[namespace] = self._files[url]['namespace']
                 load = False
             #
             # XXX Check if 'last-modified' in headers
@@ -265,14 +216,6 @@ class XmlConfig(XmlConfigParser):
             }
             self.parse(content, namespace)
         content.close()
-
-        for dst, src in self._links.items():
-            try:
-                self.link_namespace(src, dst)
-                # XXX Only do this once
-            except:
-                # The target namespace hasn't been parsed yet
-                pass
 
     def get_real_location(self, url, for_import=False):
         # XXX: Support url as a urllib.request.Request instance as well
@@ -311,10 +254,14 @@ class XmlConfig(XmlConfigParser):
 
     def parse(self, open_file, namespace):
         parser = make_parser()
-        self.push_parser(parser, namespace)
-        parser.setContentHandler(self)
+        if namespace in self.documents:
+            doc = self.documents[namespace]
+        else:
+            doc = self.documents[namespace] = XmlConfigDocument(
+                namespace=namespace, parent=self)
+        doc.parser = parser
+        parser.setContentHandler(doc)
         parser.parse(open_file)
-        self.pop_parser()
 
     def __getitem__(self, name):
         return self.lookup(name)
@@ -325,10 +272,82 @@ class XmlConfig(XmlConfigParser):
         except KeyError:
             return default
 
+    def lookup(self, key, namespace=LOCAL_NAMESPACE):
+        # XXX A regex would make more sense here
+        split = key.split(self.namespace_separator, 1)
+        if len(split) == 2:
+            # XXX override given namespace?
+            namespace=split[0]
+            key=split[1]
+
+        # Magic namespaces
+        if namespace == "env":
+            # Special lookup for environment variables
+            return os.environ[key]
+
+        # Namespace links
+        if namespace in self.links:
+            namespace = self.links[namespace]
+
+        for ns, doc in self.documents.items():
+            try:
+                return doc.lookup(key, namespace)
+            except KeyError:
+                pass
+
+        raise KeyError("{0}:{1}: Cannot lookup reference".format(namespace,key))
+
     def __iter__(self):
-        for namespace, constants in self.constants.items():
-            for key, x in constants.items():
-                yield x
+        for namespace, doc in self.documents.items():
+            for x in doc:
+                for key, y in x.items():
+                    yield y
+
+class XmlConfigDocument(XmlConfigParser):
+    """
+    Simple encapsulation of a config document. This will represent each 
+    document that is requested to be loaded or imported.
+    """
+    def __init__(self, **kwargs):
+        super(XmlConfigDocument, self).__init__(self, **kwargs)
+        self.constants = {}
+        self.default_namespace = kwargs['namespace']
+
+    def startElement(self, name, attrs):
+        if not name in self.content_types:
+            # Only enter into <constant> elements (or other registered
+            # supported content types)
+            return
+        # Keep namespace list simple
+        new_element = self.content_types[name](name=name, attrs=attrs,
+            parent=self, namespace=self.default_namespace)
+        if new_element.namespace in self.constants:
+            new_element = self.constants[new_element.namespace]
+        else:
+            self.constants[new_element.namespace] = new_element
+        self._parser.setContentHandler(new_element)
+        
+    @property
+    def parser(self):
+        return self._parser
+
+    @parser.setter
+    def parser(self, parser):
+        self._parser = parser
+
+    @property
+    def namespace(self):
+        return self.default_namespace
+       
+    @property
+    def namespaces(self):
+        return list(self.constants.keys())
+                
+    def lookup(self, key, namespace):
+        return self.constants[namespace].lookup(key)
+
+    def __iter__(self):
+        return self.constants.itervalues()
 
 @XmlConfig.register_child("constants")
 class Constants(XmlConfigParser, dict):
@@ -346,7 +365,7 @@ class Constants(XmlConfigParser, dict):
             raise ValueError("Cannot re-declare magic namespace 'env'")
         if self.options["src"] is not None:
             # Load in constants
-            getConfig(self.parent.name).load(self.options["src"], 
+            getConfig(self.root.name).load(self.options["src"], 
                 self.namespace, for_import=True)
         
     def startElement(self, name, attrs):
@@ -363,12 +382,7 @@ class Constants(XmlConfigParser, dict):
             self[new_constant.key] = new_constant
         self.parser.setContentHandler(new_constant)
         
-    def endElement(self, name):
-        self.parser.setContentHandler(self.parent)
-        
     def lookup(self, key):
-        if hasattr(self, '_link'):
-            return self.parent.lookup("{0}:{1}".format(self._link, key))
         splitkey = key.split(self.namespace_separator, 1)
         if splitkey[0] in self:
             if len(splitkey) == 2:
@@ -376,22 +390,6 @@ class Constants(XmlConfigParser, dict):
             return self[splitkey[0]]
         raise KeyError("{0}: Cannot find constant".format(key))
             
-
-    def link_to(self, namespace):
-        """
-        Used to avert circular dependencies. If a file is loaded that sources
-        a second file which sources the first file, a link will be placed to
-        link the declared namespace in the second file for the import of the
-        first file. Since the first file has already been loaded, it should
-        not be loaded a second time. If the namespace was declared different
-        from the namespace declared natively in the first file, a link 
-        between the two namespaces will suffice.
-        
-        This is confusing to describe, but it isn't a problem that will be
-        rarely triggered. 
-        XXX Insert an example
-        """
-        self._link = namespace
 
 @Constants.register_child("string")
 class SimpleConstant(XmlConfigParser):
@@ -423,11 +421,11 @@ class SimpleConstant(XmlConfigParser):
         self.parser.setContentHandler(self.children[-1])
         
     def endElement(self, name):
+        super(SimpleConstant, self).endElement(name)
         if hasattr(self, '_prev_content') \
                 and self._content != self._prev_content:
             self.on_update.fire()
             self.parent.on_update.fire(self.key)
-        self.parser.setContentHandler(self.parent)
         
     def characters(self, what):
         self._content += what
