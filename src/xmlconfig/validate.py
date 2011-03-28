221392585c399149afea9e9afa1cbba9eae2cf81
greezybacon <gravydish@gmail.com>
2011-03-28 16:39:58 -0500

    Complete operation of the WellFormedness check

--- src/xmlconfig/validate.py
+++ src/xmlconfig/validate.py
@@ -6,7 +6,7 @@ tools for now
 """
 
 from xml.sax import make_parser
-from xml.sax.handler import ErrorHandler
+from xml.sax.handler import ErrorHandler, ContentHandler
 from xml.sax.xmlreader import Locator
 from xmlconfig import getConfig, urlopen, EventHook, SimpleConstant
 from xmlconfig.cli import CliCommand
@@ -31,18 +31,22 @@ class Validator(object):
             # checks. (Fingers crossed.)
             pass
 
-    def validate_file(self, url):
+    def validate_file(self, url, namespace):
         try:
             content = urlopen(url)
         except ValueError as ex:
             self.on_error.fire(ex)
         else:
             for check in self.checks:
-                content.seek(0)
+                try:
+                    content.seek(0)
+                except:
+                    pass
                 x = check(content)
                 # Plumb the check events to the local events
                 x.on_error += self.on_error.fire
                 x.on_warning += self.on_warning.fire
+                x.check()
             content.close()
 
     @classmethod
@@ -64,24 +68,53 @@ class ValidationCheck(object):
         pass
 
 class ValidationError(Exception):
-    pass
+    """
+    Simple exception class to allow for every type of config error being
+    cast to a consistent data type for output formatting
+    """
+    def __init__(self, text, line=None, col=None, hint=None):
+        self.text = text
+        self.line = line
+        self.col= col
+        self.hint = hint
+
+    def __repr__(self):
+        return self.__unicode__()
+
+    def __unicode__(self):
+        format=""
+        args=[]
+        if self.line is not None:
+            format += "on line {0} "
+            if self.col is not None:
+                format += "col {1} "
+        format += ": {2}"
+        if self.hint is not None:
+            format += ": {3}"
+        return format.format(self.line, self.col, self.text, self.hint)
 
 @Validator.register
-class WellFormedness(ValidationCheck, ErrorHandler):
+class WellFormedness(ValidationCheck, ContentHandler, ErrorHandler):
     """
     Simple check for document well-formedness. This check will be delegated
     to the built-in Python xml parser
     """
     def check(self):
-        p = make_parser()
-        self.locator = Locator()
-        p.setDocumentLocator(self.locator)
-        p.setErrorHandler(self)
-        p.parse(self.document)
-        return iter(self. error_list)
+        try:
+            p = make_parser()
+            self.locator = Locator()
+            self.setDocumentLocator(self.locator)
+            p.setErrorHandler(self)
+            p.parse(self.document)
+        except Exception as e:
+            self.error(e)
 
     def error(self, exception):
-        self.on_error.fire(exception)
+        self.on_error.fire(ValidationError(
+            text=exception.getMessage(),
+            line=exception.getLineNumber(),
+            col=exception.getColumnNumber()
+        ))
 
     fatal_error = error
 
@@ -111,11 +144,13 @@ class ValidateConfigFile(CliCommand):
         val.on_warning += self.emit_warning
         val.validate("file:" + args[0])
 
-        print("Valiation finished with {0} errors and {1} warnings"
+        print("-------------------------------------------------------" \
+              "\nValiation finished with {0} errors and {1} warnings"
             .format(self.errors, self.warnings))
 
     def emit_warning(self, exception):
         self.warnings += 1
 
     def emit_error(self, exception):
+        print("Error " + repr(exception))
         self.errors += 1
